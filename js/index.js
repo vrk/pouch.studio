bb8704d1d709a013df7e955fc534795f5da510dc
vrk <victoriakirst@gmail.com>
2024-03-14 18:56:22 -0400

    looped

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ let msSinceAnimation = 0;
 const MsTillAnimation = 4000;
 
 const beepboop = {
+  type: "tuple",
   totalDuration: 2500,
   meBubbleAppear: 500,
   compBubbleAppear: 900,
@@ -9,9 +10,26 @@ const beepboop = {
   compBoop: document.getElementById('comp-boop')
 }
 
+const error = {
+  type: "triplet",
+  totalDuration: 3000,
+  meBubbleAppear: 500,
+  compBubbleAppear: 900,
+  meBubbleAppearAgain: 1400,
+  meBeep: document.getElementById('me-beep'),
+  compBoop: document.getElementById('comp-exclaim'),
+  meBeepAgain: document.getElementById('me-exclaim'),
+}
+
+const animations = [
+  beepboop,
+  error
+];
+
 let beepMsSinceStart = 0;
 
 let animationInProgress = false;
+let animationIndex = 0;
 
 const runloop = async () => {
 
@@ -25,19 +43,29 @@ const runloop = async () => {
   // Animation is in progress
   beepMsSinceStart += 100;
 
-  if (beepMsSinceStart === beepboop.totalDuration) {
+  const currentAnimation = animations[animationIndex];
+
+  if (beepMsSinceStart === currentAnimation.totalDuration) {
     beepMsSinceStart = 0;
     msSinceAnimation = 0;
     animationInProgress = false;
-    beepboop.meBeep.hidden = true;
-    beepboop.compBoop.hidden = true;
+    currentAnimation.meBeep.hidden = true;
+    currentAnimation.compBoop.hidden = true;
+    if (currentAnimation.meBeepAgain) {
+      currentAnimation.meBeepAgain.hidden = true;
+    }
+    animationIndex = (animationIndex + 1) % animations.length;
     return;
   }
-  if (beepMsSinceStart === beepboop.meBubbleAppear) {
-    beepboop.meBeep.hidden = false;
+  if (beepMsSinceStart === currentAnimation.meBubbleAppear) {
+    currentAnimation.meBeep.hidden = false;
   }
-  if (beepMsSinceStart === beepboop.compBubbleAppear) {
-    beepboop.compBoop.hidden = false;
+  if (beepMsSinceStart === currentAnimation.compBubbleAppear) {
+    currentAnimation.compBoop.hidden = false;
+  }
+  if (currentAnimation.meBubbleAppearAgain && currentAnimation.meBubbleAppearAgain === beepMsSinceStart) {
+    currentAnimation.meBeep.hidden = true;
+    currentAnimation.meBeepAgain.hidden = false;
   }
 }
 
